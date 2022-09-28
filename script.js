8b99bcda33176a16621026aba11344e3cbab4d04
Rachel Chen <rachelchen110@gmail.com>
2022-09-28 15:21:59 -0700

    accordion open and close animation

--- script.js
+++ script.js
@@ -3,7 +3,58 @@ let accordionButton = document.querySelector('.accordion__question');
 document.querySelectorAll('.accordion__question').forEach(item => {
     item.addEventListener('click', event => {
         console.log('click!');
-        item.classList.toggle('open');
-        item.nextElementSibling.classList.toggle('open');
+
+        let accCollapse = item.nextElementSibling;
+        if (!item.classList.contains('collapsing')) {
+            // open accordion
+            if (!item.classList.contains('open')) {
+                console.log('toggle accordion button');
+                
+                accCollapse.style.display = 'block';
+                let accHeight = accCollapse.clientHeight;
+
+                setTimeout(() => {
+                    accCollapse.style.height = accHeight+'px';
+                    accCollapse.style.display = '';
+                }, 1);
+                
+                accCollapse.classList = 'accordion__collapse collapsing';
+
+
+                // After X amount of time, remove collapsing class and add "collapse open" class
+                setTimeout(() => {
+                    console.log('open accordion content');
+                    accCollapse.classList = 'accordion__collapse collapse open';
+                    
+
+                }, 300);
+
+            }
+            // Close accordion item
+            else {
+                // Remove "collapse open" from .accordion__collapse, add "collapsing"
+                accCollapse.classList = 'accordion__collapse collapsing';
+
+                setTimeout(() => {
+                    accCollapse.style.height = '0px';
+                }, 1);
+                // After X amount of time, remove "collapsing" class and add "collapse" class
+                setTimeout(() => {
+                    console.log('close accordion content');
+
+                    accCollapse.classList = 'accordion__collapse collapse';
+                    accCollapse.style.height = '';
+
+                }, 300);
+
+            }
+
+            item.classList.toggle('open');
+        }
+        
+
+        
+
+        
     });
 });
